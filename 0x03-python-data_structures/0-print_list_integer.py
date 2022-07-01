63bb46acce47b18dfce36cf395136b5d0ee38cfe
Damiennsoh <psycaro20@gmail.com>
2022-07-01 09:48:50 -0700

    A function that prints all integer of a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    if len(matrix) > 0:
-        for elems in matrix[:]:
-            new_matrix.append(list(map(lambda x: x ** 2, elems)))
-
-    return new_matrix
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print('{:d}'.format(i))
